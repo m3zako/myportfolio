fcd83677e98507146c7ac6c38972fdbd58e962d7
Jonah Halili <j0halili00@gmail.com>
2023-06-18 16:11:01 -0400

    tweaked formatting and responsiveness

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ const App = () => {
   };
 
   return (
-    <>
+    <portfolio className="bg-gray-100 min-h-screen">
       <header className="relative">
         <nav className="fixed w-full h-10">
           <ul className="flex justify-end mr-6 mt-5">
@@ -133,22 +133,22 @@ const App = () => {
             </li>
           </ul>
         </nav>
-        <section className="flex flex-col text-gray-800 items-center justify-center min-h-screen bg-gray-100">
-          <div className="w-full lg:w-1/2 md:w-full flex flex-col lg:flex-row items-center justify-center lg:px-5">
-            <motion.div className="mx-5 md:px-5"
-            initial={{opacity: 0}}
-            animate={{opacity: 1}}
-            transition={{duration: 0.5, delay: 1.5}}
+        <intro className="flex flex-col text-gray-800 items-center justify-center min-h-screen bg-gray-100">
+          <div className="max-w-screen-lg flex flex-col lg:flex-row items-center justify-center">
+            <motion.div className="mx-10 md:px-5"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 1.5 }}
             >
-              <h1 className="text-4xl mb-4 font-bold">Hello! I'm Jonah.</h1>
-              <p className="text-2xl text-justified mb-8">I'm a software engineer and developer with a creative passion for entertaining user experiences.</p>
-              <p className="text-xl mb-8">Welcome to my website portfolio!</p>
+              <h1 className="text-4xl lg:text-4xl mb-4 font-bold">Hello! I'm Jonah.</h1>
+              <p className="text-2xl lg:text-2xl text-justified mb-8">I'm a software engineer and developer with a creative passion for entertaining user experiences.</p>
+              <p className="text-xl lg:text-xl mb-8">Welcome to my website portfolio!</p>
             </motion.div>
             <div className="w-full md:w-auto sm:w-auto flex-shrink-0">
               <motion.img src={joni} alt="Profile" className="mx-auto sm:mx-0"
-                initial={{y:"300%"}}
-                animate={{y:"0%"}}
-                transition={{type:"spring", duration:"1.5"}}
+                initial={{ y: "300%" }}
+                animate={{ y: "0%" }}
+                transition={{ type: "spring", duration: "1.5" }}
               />
             </div>
             <div className="absolute bottom-0 left-1/2 -translate-x-1/2" id="projects">
@@ -157,105 +157,113 @@ const App = () => {
               </svg>
             </div>
           </div>
-        </section>
+        </intro>
       </header>
-      <div className="bg-gray-100 text-gray-800">
-        <div className="flex flex-col items-center justify-center mb-40 xl:mx-96 lg:px-10 md:mx-10">
-          <h2 className="text-xl ld mb-8 text-gray-800 px-10">hover over any of these projects to learn a little more about them!</h2>
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 mx-10">
-            <div className="relative overflow-hidden rounded-xl">
-              <motion.img src={project1} alt="Animation" initial={{x:-200}} whileInView={{x:0, zIndex:0, transition:{type:"spring", duration:1}}} viewport={{once:true}} style={{zIndex:1}}/>
-              <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100" style={{zIndex:1}}>
-                <div className="text-white text-center text-xs lg:text-sm xl:text-lg lg:px-4">
-                  <h1 className="text-lg font-bold mb-1">AR Glasses</h1>
-                  <p className="text-md mx-2 my-2"> Worked on a heads up display and an Android application for consumer level hardware.</p>
-                  <div className="flex flex-wrap justify-center mb-4">
-                    <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2 mb-2">
-                    Java
-                    </li>
-                    <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2 mb-2">
-                      Android Studio
-                    </li>
-                    <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2 mb-2">
-                      Docker
-                    </li>
-                  </div>
-                  <a href="https://imgur.com/a/DpAA6dY" rel="noreferrer" target="_blank" className="underline">View Project</a>
-                </div>
-              </div>
-            </div>
-            <div className="relative overflow-hidden rounded-xl">
-              <motion.img src={project2} alt="Project 2" initial={{x:200}} whileInView={{x:0, transition:{type:"spring", duration:1}}} viewport={{once:true}}/>
-              <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100">
-                <div className="text-white text-center text-xs lg:text-sm">
-                  <h1 className="text-lg font-bold mb-2">Programmable Trackpad</h1>
-                  <p className="mx-2 my-2"> Designed and developed a peripheral complete with macros, rotary encoders, and mouse funtionality from the ground up.</p>
-                  <div className="flex flex-wrap justify-center mb-4">
-                    <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2">
-                      Arduino
-                    </li>
-                    <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2 ">
-                      C
-                    </li>
-                    <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full ">
-                      Python
-                    </li>
-                  </div>
-                  <a href="https://programmabletrackpad-group18.web.app/" rel="noreferrer" target="_blank" className="underline text-sm">View Project</a>
-                </div>
-              </div>
-            </div>
-            <div className="relative overflow-hidden rounded-xl">
-              <motion.img src={project3} alt="Project 3" initial={{x:-200}} whileInView={{x:0, transition: {type: "spring", duration: 0.8}}} viewport={{once:true}}/>
-              <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100">
-                <div className="text-white text-center text-xs lg:text-sm">
-                  <h1 className="text-lg font-bold mb-1">Energy Solutions</h1>
-                  <p className="mx-2 my-2 lg:mx-8"> Conducted a reserach to propose an energy efficient solution to Duke Energy. 
-                    Curated a refined genetic algorithm using a neural network to find the optimal chemical formual for silicon insulators.</p>
+      <projects>
+        <div className="bg-gray-100 text-gray-800 flex justify-center">
+          <div className="flex flex-col items-center justify-center mb-40 mx-auto max-w-4xl md:mx-10">
+            <h2 className="text-xl ld mb-8 text-gray-800 px-10">hover over any of these projects to learn a little more about them!</h2>
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 mx-10">
+              <div className="relative overflow-hidden rounded-xl">
+                <motion.img src={project1} alt="Animation" initial={{x:-200}} whileInView={{x:0, zIndex:0, transition:{type:"spring", duration:1}}} viewport={{once:true}} style={{zIndex:1}}/>
+                <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100" style={{zIndex:1}}>
+                  <div className="text-white text-center text-xs lg:text-sm xl:text-lg px-auto mx-auto">
+                    <h1 className="text-lg font-bold mb-1">AR Glasses</h1>
+                    <p className="text-md mx-2 my-2"> Worked on a heads up display and an Android application for consumer level hardware.</p>
                     <div className="flex flex-wrap justify-center mb-4">
-                    <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2">
-                      Python
-                    </li>
-                    <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2 ">
-                      Tensorflow
-                    </li>
+                      <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2 mb-2">
+                      Java
+                      </li>
+                      <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2 mb-2">
+                        Android Studio
+                      </li>
+                      <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2 mb-2">
+                        Docker
+                      </li>
+                    </div>
+                    <a href="https://imgur.com/a/DpAA6dY" rel="noreferrer" target="_blank" className="underline">View Project</a>
                   </div>
-                  <a href="https://imgur.com/WpQUj8z" rel="noreferrer" target="_blank"className="underline text-sm">View Project</a>
                 </div>
               </div>
-            </div>
-            <div className="relative overflow-hidden rounded-xl">
-              <motion.img src={project4} alt="Project 4" initial={{x:200}} whileInView={{x:0, transition: {type: "spring", duration: 1}}} viewport={{once:true}}/>
-              <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100">
-                <div className="text-white text-center md:text-sm lg:text-lg">
-                  <h1 className="text-lg font-bold mb-2">PocketPantry</h1>
-                  <p className="mx-2 my-4 px-8"> An app for finding recipes and a checklist for grocery shopping.</p><div className="flex flex-wrap justify-center mb-4">
-                    <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2">
-                      MERN
-                    </li>
+              <div className="relative overflow-hidden rounded-xl">
+                <motion.img src={project2} alt="Project 2" initial={{x:200}} whileInView={{x:0, transition:{type:"spring", duration:1}}} viewport={{once:true}}/>
+                <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100">
+                  <div className="text-white text-center text-xs lg:text-sm px-auto mx-auto">
+                    <h1 className="text-lg font-bold mb-2">Programmable Trackpad</h1>
+                    <p className="mx-2 my-2"> Designed and developed a peripheral complete with macros, rotary encoders, and mouse funtionality from the ground up.</p>
+                    <div className="flex flex-wrap justify-center mb-4">
+                      <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2">
+                        Arduino
+                      </li>
+                      <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2 ">
+                        C
+                      </li>
+                      <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full ">
+                        Python
+                      </li>
+                    </div>
+                    <a href="https://programmabletrackpad-group18.web.app/" rel="noreferrer" target="_blank" className="underline text-sm">View Project</a>
                   </div>
-                  <a href="https://imgur.com/a/FRc3ZNk" rel="noreferrer" target="_blank" className="underline">View Project</a>
+                </div>
+              </div>
+              <div className="relative overflow-hidden rounded-xl">
+                <motion.img src={project3} alt="Project 3" initial={{x:-200}} whileInView={{x:0, transition: {type: "spring", duration: 0.8}}} viewport={{once:true}}/>
+                <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100">
+                  <div className="text-white text-center text-xs lg:text-sm px-auto mx-auto">
+                    <h1 className="text-lg font-bold mb-1">Energy Solutions</h1>
+                    <p className="mx-2 my-2 lg:mx-8"> Conducted a research to propose an energy efficient solution to Duke Energy. 
+                      Curated a refined genetic algorithm using a neural network to find the optimal chemical formual for silicon insulators.</p>
+                      <div className="flex flex-wrap justify-center mb-4">
+                      <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2">
+                        Python
+                      </li>
+                      <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2 ">
+                        Tensorflow
+                      </li>
+                    </div>
+                    <a href="https://imgur.com/WpQUj8z" rel="noreferrer" target="_blank"className="underline text-sm">View Project</a>
+                  </div>
+                </div>
+              </div>
+              <div className="relative overflow-hidden rounded-xl">
+                <motion.img src={project4} alt="Project 4" initial={{x:200}} whileInView={{x:0, transition: {type: "spring", duration: 1}}} viewport={{once:true}}/>
+                <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100">
+                  <div className="text-white text-center md:text-sm lg:text-lg px-auto mx-auto">
+                    <h1 className="text-lg font-bold mb-2">PocketPantry</h1>
+                    <p className="mx-2 my-4 px-8"> An app for finding recipes and a checklist for grocery shopping.</p><div className="flex flex-wrap justify-center mb-4">
+                      <li className="inline-block bg-gray-500 text-white py-1 px-3 rounded-full mr-2">
+                        MERN
+                      </li>
+                    </div>
+                    <a href="https://imgur.com/a/FRc3ZNk" rel="noreferrer" target="_blank" className="underline">View Project</a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <motion.div className="flex flex-col text-gray-800 items-center justify-center mb-32 bg-gray-100" initial={{y:300, opacity: 0}} whileInView={{y:0, opacity: 1, transition:{type:"spring", duration:1}}} id="about" viewport={{once:true}}>
-          <div className="w-full xl:w-1/2 lg:w-full md:w-full sm:w-1/2 flex flex-col lg:flex-row items-center lg:px-5">
-            <div className="text-center lg:text-left sm:text-center mx-8">
-              <h1 className="text-3xl mb-4 font-bold">About Me</h1>
-              <p className="text-xl text-justify mb-8 pr-0 lg:pr-4 lg:px-0 md:px-10">I'm a '23 graduate from the University of Central Florida with a B.S. in Computer Engineering. <br/> <br/>
-                Aside from coding, most of my time goes towards the abundance of hobbies I have ranging from making music, building keyboards and gundams.<br/><br/>
-              </p>
+      </projects>
+      <about className="bg-gray-100 flex flex-col items-center justify-center">
+        <div className="w-full px-auto flex flex-col lg:flex-row items-center justify-center max-w-screen-lg mb-32">
+          <motion.div className="text-gray-800 items-center justify-center" initial={{y:300, opacity: 0}} whileInView={{y:0, opacity: 1, transition:{type:"spring", duration:1}}} id="about" viewport={{once:true}}>
+            <div className="w-full flex flex-col lg:flex-row items-center lg:px-5">
+              <div className="text-center lg:text-left sm:text-center mx-8">
+                <h1 className="text-3xl mb-4 font-bold">About Me</h1>
+                <p className="text-xl text-justify mb-8 ">I'm a '23 graduate from the University of Central Florida with a B.S. in Computer Engineering. <br/> <br/>
+                  Aside from coding, most of my time goes towards the abundance of hobbies I have ranging from making music, building keyboards and gundams.<br/><br/>
+                </p>
+              </div>
+              <div className="w-1/2 max-w-md flex-shrink-0">
+                <img src={headshot} alt="Profile" className="rounded-3xl" />
+              </div>
             </div>
-            <div className="w-1/2 h-1/2 lg:w-1/3 lg:h-1/3 flex-shrink-0">
-              <img src={headshot} alt="Profile" className="rounded-3xl" />
-            </div>
-          </div>
-        </motion.div>
-        <div className="flex items-center justify-center text-gray-800 mb-32 bg-gray-100 mx-8">
-          <motion.div className="w-full xl:w-1/2 lg:w-full md:w-full sm:w-1/2 px-10" initial={{y:300, opacity: 0}} whileInView={{y:0, opacity: 1, transition:{type:"spring", duration:1}}} viewport={{once:true}}>
-            <h1 className="text-3xl text-center lg:text-left sm:text-center mb-8 font-bold">Skills</h1>
+          </motion.div>
+        </div>
+      </about>
+      <section className="bg-gray-100 min-h-0 flex-col items-center justify-center border-2 border-gray-100">
+        <div className="items-center justify-center text-gray-800 mb-32 bg-gray-100 w-full">
+          <motion.div initial={{y:300, opacity: 0}} whileInView={{y:0, opacity: 1, transition:{type:"spring", duration:1}}} viewport={{once:true}}>
+            <h1 className="text-3xl lg:text-left sm:text-center mb-8 font-bold  max-w-screen-lg mx-auto text-center px-14">Skills</h1>
             <div className="flex flex-wrap md:flex-wrap sm:flex-row items-center justify-center" >
               <div className="text-center text-md flex flex-col items-center px-4">
                 <FaJava className="w-16 h-16 mb-2" />
@@ -288,9 +296,11 @@ const App = () => {
             </div>
           </motion.div>
         </div>
-        <footer className="flex flex-col items-center justify-center text-gray-800 bg-gray-100 mx-10" id="connect">
-          <motion.div className=" xl:w-1/2 lg:w-full md:w-full flex flex-col lg:flex-row mb-40 md:px-8" initial={{y:300, opacity: 0}} whileInView={{y:0, opacity: 1, transition:{type:"spring", duration:1}}} viewport={{once:true}}>
-            <div>
+      </section>
+      <section className="bg-gray-100 min-h-0">
+        <footer className="max-w-screen-lg mx-auto border-2 border-gray-100" id="connect">
+          <motion.div className=" flex flex-col lg:flex-row mb-40 md:px-8" initial={{y:300, opacity: 0}} whileInView={{y:0, opacity: 1, transition:{type:"spring", duration:1}}} viewport={{once:true}}>
+            <div className="px-6">
               <h1 className="text-3xl mb-4 text-center lg:text-left sm:text-center font-bold">Let's Connect!</h1>
               <p className="text-xl text-center lg:text-left md:text-center mb-8 lg:px-0"> If you'd like to connect, feel free to reach out!</p>
               <div className="flex flex-row px-4 items-center justify-center lg:items-left">
@@ -328,8 +338,8 @@ const App = () => {
             </div>
           </motion.div>
         </footer>
-      </div>
-    </>
+      </section>
+    </portfolio>
   );
 }
 
